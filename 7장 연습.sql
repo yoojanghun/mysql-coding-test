869ccff768c162846397e7d0b41bbc774afa4393
banana-coder631 <157185025+yoojanghun@users.noreply.github.com>
2025-06-07 23:50:24 +0900

    7장 연습 - 2

--- 7장 연습.sql
+++ 7장 연습.sql
@@ -156,3 +156,56 @@ WITH RECURSIVE employee_anchor (Ssn, Fname, Minit, Lname, Level) AS
 )
     SELECT *
 FROM employee_anchor; /* ORDER BY 없어도 레벨 순으로 출력됨 */
+
+
+-- 'Ramesh Narayan'의 관리자부터 최상위 관리자까지 모든 관리자를 직급별로 순서대로 검색 (하위부터 검색)
+WITH RECURSIVE employee_anchor (Ssn, Fname, Minit, Lname, Super_ssn, Level) AS
+(
+	SELECT	Ssn, Fname, Minit, Lname, Super_ssn, 1
+    FROM	EMPLOYEE
+    WHERE	Fname = 'Ramesh' AND Lname = 'Narayan'
+    UNION	ALL
+    SELECT	E.Ssn, E.Fname, E.Minit, E.Lname, E.Super_ssn, Level+1
+    FROM	employee_anchor EA 
+			JOIN EMPLOYEE E ON EA.SUPER_SSN = E.SSN
+)
+SELECT	*
+FROM	employee_anchor;
+
+
+-- 원시 데이터 테이블: 한 투플에 한 선수의 정보를 기록
+-- 통계 요약 테이블: 포지션 별로 선수가 몇 명인지 기록
+
+-- 가로축: CASE 표현식을 사용하여, 피벗 컬럼에 있는 유일한 값의 개수만큼, 컬럼을 생성
+-- 세로축: 그룹핑 기준 컬럼에 있는 유일한 값의 개수만큼, 행을 생성
+-- 셀: 그룹 별로, 생성한 컬럼에 집계함수를 적용하여 원하는 통계치를 계산
+
+
+-- 팀별/포지션별 선수 수를 검색 (세로축: 팀, 가로축: 포지션)
+-- 1. 원시 데이터 테이블에서 PIVOT시킬 데이터 테이블 생성
+SELECT	PLAYER_NAME, TEAM_ID, POSITION
+FROM	PLAYER;
+
+
+-- 2.피벗 컬럼을 PIVOT시킴
+SELECT	TEAM_ID,
+		CASE WHEN POSITION = 'FW' THEN 1 END FW,
+        CASE WHEN POSITION = 'MF' THEN 1 END MF,
+        CASE WHEN POSITION = 'DF' THEN 1 END DF,
+        CASE WHEN POSITION = 'GK' THEN 1 END GK,
+        CASE WHEN POSITION IS NULL THEN 1 END UNDECIDED
+FROM	PLAYER;
+
+
+-- GROUP BY절 집계 함수로 통계치(선수 수)생성
+SELECT	TEAM_ID,
+		COALESCE(SUM(CASE WHEN POSITION = 'FW' THEN 1 END),0) FW,
+        COALESCE(SUM(CASE WHEN POSITION = 'MF' THEN 1 END),0) MF,
+        COALESCE(SUM(CASE WHEN POSITION = 'DF' THEN 1 END),0) DF,
+        COALESCE(SUM(CASE WHEN POSITION = 'GK' THEN 1 END),0) GK,
+        COALESCE(SUM(CASE WHEN POSITION IS NULL THEN 1 END),0) UNDECIDED,
+        
+        COUNT(PLAYER_NAME) '팀별 선수수'
+FROM	PLAYER
+GROUP 	BY TEAM_ID
+ORDER 	BY TEAM_ID;
